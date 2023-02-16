a6b5002959566ce038b3688608bb60d5752e405b
Abián Hernández <abianhernandez@outlook.es>
2023-02-16 14:43:11 +0000

    HySpecLab module

---
+++ config.py
@@ -0,0 +1,8 @@
+import os, sys
+
+# Dataset path
+DATASET_PATH = '/home/abian/Data/Dataset/HSI/'
+JasperRidge_PATH = os.path.join(DATASET_PATH, 'JasperRidge') 
+Samson_PATH = os.path.join(DATASET_PATH, 'Samson')
+Urban_PATH = os.path.join(DATASET_PATH, 'Urban')
+
