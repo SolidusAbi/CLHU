4de49c8e44e56183aa5a2d3890a2258c3f6269fe
Abián Hernández <abianhernandez@outlook.es>
2023-02-20 17:28:21 +0000

    First test validating the unmixing model with IT.

--- config.py
+++ config.py
@@ -11,3 +11,7 @@ project_root_dir = os.path.dirname(__file__)
 hyspeclab_dir = os.path.join(project_root_dir, 'modules/HySpecLab')
 if hyspeclab_dir not in sys.path:
     sys.path.append(hyspeclab_dir)
+
+ipdl_dir = os.path.join(project_root_dir, 'modules/IPDL')
+if ipdl_dir not in sys.path:
+    sys.path.append(ipdl_dir)
