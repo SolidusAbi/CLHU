a7d9f5c046a2ff4df4b8c972f97ef44512b1e34e
Abián Hernández <abianhernandez@outlook.es>
2023-03-01 15:29:05 +0000

    ...

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os, sys
+project_root_dir = os.path.dirname(__file__)
 
 # Dataset path
 DATASET_PATH = '/home/abian/Data/Dataset/HSI/'
@@ -6,8 +7,11 @@ JasperRidge_PATH = os.path.join(DATASET_PATH, 'JasperRidge')
 Samson_PATH = os.path.join(DATASET_PATH, 'Samson')
 Urban_PATH = os.path.join(DATASET_PATH, 'Urban')
 
+# Results path
+RESULTS_PATH = os.path.join(project_root_dir, 'data/results/')
+IMG_PATH = os.path.join(project_root_dir, 'data/img/')
+
 # Dependencies
-project_root_dir = os.path.dirname(__file__)
 hyspeclab_dir = os.path.join(project_root_dir, 'modules/HySpecLab')
 if hyspeclab_dir not in sys.path:
     sys.path.append(hyspeclab_dir)
---
+++ notebooks/dataset.py
@@ -0,0 +1,73 @@
+import os
+import torch
+from torch import tensor
+from torch.utils.data import Dataset
+import scipy.io as sio
+import numpy as np
+
+
+class HSIDataset(Dataset):
+    def __init__(self):
+        super(HSIDataset, self).__init__()
+
+    def preprocessing(self, X:np.ndarray):
+        '''
+            Preprocessing the dataset for removing high-frequency noise. 
+            This preprocessing consists of three steps:
+                1. Median filter in the spatial domain.
+                2. Moving average filter in the spectral domain.
+                3. Normalization of the data.
+
+            Parameters
+            ----------
+                X : np.ndarray, shape (nRow, nCol, nBand)
+                    HSI Cube.
+        '''
+
+        from skimage.filters import median
+        from utils import moving_average
+
+        max_value = X.max()
+
+        X = median(X, footprint=np.ones((3,3,1)))
+        # X = moving_average(X.reshape(-1, X.shape[-1]), 3, padding_size=2).reshape(X.shape[0], X.shape[1], -1)
+        X = X / (max_value + 1e-3)
+        return X
+
+class JasperRidge(HSIDataset):
+    def __init__(self, root_dir, transform=None):
+        super(JasperRidge, self).__init__()
+        data = sio.loadmat(os.path.join(root_dir, 'jasperRidge2_R198.mat'))
+        y = sio.loadmat(os.path.join(root_dir, 'GroundTruth/end4.mat'))
+
+        self.n_row, self.n_col = data['nRow'].item(), data['nCol'].item()
+
+        self.X = data['Y'].T.reshape(self.n_row, self.n_col, -1) # (nRow, nCol, nBand)
+        self.X = self.preprocessing(self.X).reshape(-1, self.X.shape[-1]) # (nRow*nCol, nBand)
+        self.X = tensor(self.X, dtype=torch.float32)
+
+        self.E = tensor(y['M'].T, dtype=torch.float32) # (nEndmember, nBand)
+        self.A = tensor(y['A'].T, dtype=torch.float32) # (nRow*nCol, nEndmember)
+        self.n_bands = self.X.shape[1]
+        self.n_endmembers = self.E.shape[0]
+        
+        self.transform = transform
+
+    def __len__(self):
+        return self.n_row * self.n_col
+
+    def __getitem__(self, idx):
+        sample = self.X[idx]
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def endmembers(self):
+        return self.E
+
+    def abundance(self):
+        return self.A.reshape(self.n_row, self.n_col, -1)
+
+    def image(self):
+        return self.X.reshape(self.n_row, self.n_col, -1)
