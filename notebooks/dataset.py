06fd4bf83bfe2082c12a164d8939ce7d8d08f94f
Abián Hernández <abianhernandez@outlook.es>
2023-03-03 16:34:17 +0000

    Samson and Urban dataset implemented and tested

--- notebooks/dataset.py
+++ notebooks/dataset.py
@@ -34,6 +34,7 @@ class HSIDataset(Dataset):
         X = X / (max_value + 1e-3)
         return X
 
+# Jasper Ridge dataset
 class JasperRidge(HSIDataset):
     def __init__(self, root_dir, transform=None):
         super(JasperRidge, self).__init__()
@@ -49,6 +50,86 @@ class JasperRidge(HSIDataset):
         self.E = tensor(y['M'].T, dtype=torch.float32) # (nEndmember, nBand)
         self.A = tensor(y['A'].T, dtype=torch.float32) # (nRow*nCol, nEndmember)
         self.n_bands = self.X.shape[1]
+        self.n_endmembers = self.E.shape[0]
+        
+        self.transform = transform
+
+    def __len__(self):
+        return self.n_row * self.n_col
+
+    def __getitem__(self, idx):
+        sample = self.X[idx]
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def endmembers(self):
+        return self.E
+
+    def abundance(self):
+        return self.A.reshape(self.n_row, self.n_col, -1)
+
+    def image(self):
+        return self.X.reshape(self.n_row, self.n_col, -1)
+    
+# Samson dataset
+class Samson(HSIDataset):
+    def __init__(self, root_dir, transform=None):
+        super(Samson, self).__init__()
+
+        data = sio.loadmat(os.path.join(root_dir, 'Data_Matlab/samson_1.mat'))
+        y = sio.loadmat(os.path.join(root_dir, 'GroundTruth/end3.mat'))
+
+
+        self.n_row, self.n_col , self.n_bands = data['nRow'].item(), data['nCol'].item(), data['nBand'].item()
+
+        self.X = data['V'].T.reshape(self.n_row, self.n_col, -1) # (nRow, nCol, nBand)
+        self.X = self.preprocessing(self.X).reshape(-1, self.X.shape[-1]) # (nRow*nCol, nBand)
+        self.X = tensor(self.X, dtype=torch.float32)
+
+        self.E = tensor(y['M'].T, dtype=torch.float32) # (nEndmember, nBand)
+        self.A = tensor(y['A'].T, dtype=torch.float32) # (nRow*nCol, nEndmember)
+        self.n_endmembers = self.E.shape[0]
+        
+        self.transform = transform
+
+    def __len__(self):
+        return self.n_row * self.n_col
+
+    def __getitem__(self, idx):
+        sample = self.X[idx]
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+    def endmembers(self):
+        return self.E
+
+    def abundance(self):
+        return self.A.reshape(self.n_row, self.n_col, -1)
+
+    def image(self):
+        return self.X.reshape(self.n_row, self.n_col, -1)
+    
+# Urban dataset
+class Urban(HSIDataset):
+    def __init__(self, root_dir, transform=None):
+        super(Urban, self).__init__()
+
+        data = sio.loadmat(os.path.join(root_dir, 'Urban_R162.mat'))
+        y = sio.loadmat(os.path.join(root_dir, 'groundTruth/end4_groundTruth.mat'))
+
+
+        self.n_row, self.n_col , self.n_bands = data['nRow'].item(), data['nCol'].item(), data['nBand'].item()
+
+        self.X = data['Y'].T.reshape(self.n_row, self.n_col, -1) # (nRow, nCol, nBand)
+        self.X = self.preprocessing(self.X).reshape(-1, self.X.shape[-1]) # (nRow*nCol, nBand)
+        self.X = tensor(self.X, dtype=torch.float32)
+
+        self.E = tensor(y['M'].T, dtype=torch.float32) # (nEndmember, nBand)
+        self.A = tensor(y['A'].T, dtype=torch.float32) # (nRow*nCol, nEndmember)
         self.n_endmembers = self.E.shape[0]
         
         self.transform = transform
