79467f6057c668a779d0de4cc56244185889e199
Abian Hernandez <abianhernandez@outlook.es>
2023-04-10 17:35:34 +0100

    utils:plot_endmember(), now it is posible to represent different estimation of the same endmember.

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -22,7 +22,9 @@ def moving_average(a:np.ndarray, n=3, padding_size=2):
     return ret[:, n - 1:] / n
 
 from matplotlib import pyplot as plt
-def plot_endmembers(E: np.ndarray, wv:np.ndarray = None, labels:list = None, figsize:tuple = (7,5), ticks_range:tuple=(0, 1), n_ticks:int=5):
+def plot_endmembers(E: np.ndarray, wv:np.ndarray = None, labels:list = None, figsize:tuple = (7,5), 
+                    ticks_range:tuple=(0, 1), n_ticks:int=5, 
+                    endmember_estimation:list = None, ee_labels:list = None):
     '''
         Plot endmembers.
 
@@ -40,6 +42,12 @@ def plot_endmembers(E: np.ndarray, wv:np.ndarray = None, labels:list = None, fig
                 Range of yticks. Default is (0, 1).
             n_ticks : int, optional
                 Number of yticks. Default is 5.
+            endmember_estimation : list, optional
+                List of arrys which contains the endmembers estimation
+                by different algorithms. Default is None. Max length: 3.
+            ee_labels : list, optional
+                Labels for the algorithms used in the endmembers estimation.
+                Default is None.
     '''
     ticks_formatter = plt.FormatStrFormatter('%.2f')
 
@@ -48,6 +56,7 @@ def plot_endmembers(E: np.ndarray, wv:np.ndarray = None, labels:list = None, fig
         labels = list(map(lambda x: r'$E_{{{}}}$'.format(x), range(1, n_endmembers+1)))
 
     with plt.style.context(("seaborn-colorblind")):
+        colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
         fig, ax = plt.subplots(1, 1, figsize=figsize)
         ticks = np.linspace(*ticks_range, n_ticks)
 
@@ -73,7 +82,42 @@ def plot_endmembers(E: np.ndarray, wv:np.ndarray = None, labels:list = None, fig
         fig.legend(handles, labels, loc='upper center', ncol=6, fontsize='large', borderpad=-.25)
         fig.tight_layout(pad=(((n_endmembers-1)//6)+1)*2) # padding based on the endmembers number
 
+        if endmember_estimation is not None and ee_labels is not None:
+            ax = include_estimation(ax, colors, endmember_estimation, ee_labels)
+
     return fig
+
+def include_estimation(ax, colors, endmember_estimation:list, labels:list):
+    '''
+        Include endmembers estimation in a plot.
+
+        Parameters
+        ----------
+            ax : matplotlib.axes._subplots.AxesSubplot
+                Axes object.
+            colors : list
+                List of colors for the endmembers.
+            endmember_estimation : list, max length: 3
+                List of arrys which contains the endmembers estimation
+                by different algorithms.
+            labels : list
+                Labels for the algorithms used in the endmembers estimation.
+    '''
+    styles = ['-', '--', ':', '-.']
+    labels = ['Ground Truth'] + labels
+    n_endmembers, _ = endmember_estimation[0].shape
+    for i, endmember in enumerate(endmember_estimation):
+        for j in range(n_endmembers):
+            ax.plot(endmember_estimation[i][j], styles[i+1], color=colors[j])
+
+    _ax = ax.twinx()
+    for ss, sty in enumerate(labels):
+        _ax.plot(np.NaN, np.NaN, ls=styles[ss],
+                label=labels[ss], c=colors[0])
+    _ax.get_yaxis().set_visible(False)
+    _ax.legend(loc='upper left', fontsize='large')
+
+    return _ax
 
 def show_abundance(A, labels:list = None, figsize:tuple=(7,5)):
     '''
