6923d9377f6b33978eaaf76638d134b785fa3c4e
Abián Hernández <abianhernandez@outlook.es>
2024-03-01 16:45:00 +0000

    CLHU v2, New test

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,25 +1,32 @@
 import numpy as np
 
-def moving_average(a:np.ndarray, n=3, padding_size=2):
-    ''' 
-        Moving average filter for 1-D array.
+# def moving_average(a:np.ndarray, n=3, padding_size=2):
+#     ''' 
+#         Moving average filter for 1-D array.
 
-        Parameters
-        ----------
-        a : 1-D array, shape (batch_size, n_features)
-            Input array.
-        n : int, optional
-            Window size. Default is 3.
-        padding_size : int, optional
-            Padding size. Default is 2.
+#         Parameters
+#         ----------
+#         a : 1-D array, shape (batch_size, n_features)
+#             Input array.
+#         n : int, optional
+#             Window size. Default is 3.
+#         padding_size : int, optional
+#             Padding size. Default is 2.
     
-    '''
-    if padding_size > 0:
-        a = np.pad(a, ((0,0), (padding_size//2, padding_size//2)), 'edge')
+#     '''
+#     if padding_size > 0:
+#         a = np.pad(a, ((0,0), (padding_size//2, padding_size//2)), 'edge')
 
-    ret = np.cumsum(a, axis=1, dtype=float)
-    ret[:, n:] = ret[:, n:] - ret[:, :-n]
-    return ret[:, n - 1:] / n
+#     ret = np.cumsum(a, axis=1, dtype=float)
+#     ret[:, n:] = ret[:, n:] - ret[:, :-n]
+#     return ret[:, n - 1:] / n
+
+def moving_average(a, n=10, padding_size=0) :
+    # ret = np.cumsum(a, dtype=float) if padding_size == 0 else np.cumsum(np.insert(a, 0, np.zeros(padding_size)), dtype=float)
+    ret = np.cumsum(a, dtype=float) if padding_size == 0 else np.cumsum(np.insert(a, 0, np.ones(padding_size)*a[0]), dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
 
 from matplotlib import pyplot as plt
 def plot_endmembers(E: np.ndarray, wv:np.ndarray = None, labels:list = None, figsize:tuple = (7,5), 
--- utils/Training.py
+++ utils/Training.py
@@ -5,7 +5,7 @@ from torch import sigmoid, nn
 from tqdm import tqdm
 import torch
 
-def train(model:nn.Module, n_endmembers:int, dataset:Dataset, n_batchs:int = 64, n_epochs:int = 100, lr=1e-3, similarity_weight=.5, simplex_weight=1e-5):
+def train(model:nn.Module, n_endmembers:int, dataset:Dataset, n_batchs:int = 64, n_epochs:int = 100, lr=1e-3, similarity_weight=1, simplex_weight=1e-5):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model = model.to(device)
     model.train()
@@ -15,8 +15,11 @@ def train(model:nn.Module, n_endmembers:int, dataset:Dataset, n_batchs:int = 64,
 
     criterion = UnmixingLoss() 
     volume_reg = SimplexVolumeLoss(dataset[:], n_endmembers).to(device)
+    # similarity_reg = SimilarityLoss(n_endmembers, temperature=.1, reduction='mean')
     similarity_reg = SimilarityLoss(n_endmembers, temperature=.1, reduction='mean')
+
     vol_reg_norm = volume_reg(torch.sigmoid(model.ebk.detach()))
+
 
     dataloader = DataLoader(dataset, batch_size=int(len(dataset)/n_batchs), shuffle=True)
 
@@ -37,6 +40,7 @@ def train(model:nn.Module, n_endmembers:int, dataset:Dataset, n_batchs:int = 64,
             x = x.to(device)
             optimizer.zero_grad()
             y = model(x)
+            # loss = criterion(y, x) + simplex_weight*(volume_reg(sigmoid(model.ebk))/vol_reg_norm) + similarity_weight*similarity_reg(model.ebk)
             loss = criterion(y, x) + simplex_weight*(volume_reg(sigmoid(model.ebk))/vol_reg_norm) + similarity_weight*similarity_reg(model.ebk)
             epoch_loss += loss.detach().item()
 
